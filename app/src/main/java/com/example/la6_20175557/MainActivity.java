68b6d86282a462e5910a4817c98ee9526a571cab
caleb <caleb.casapaico@pucp.edu.pe>
2024-06-10 01:25:08 -0500

    avance_please

--- app/src/main/java/com/example/la6_20175557/MainActivity.java
+++ app/src/main/java/com/example/la6_20175557/MainActivity.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.la6_20175557.databinding.ActivityMainBinding;
 import com.example.la6_20175557.dto.egresos;
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ListenerRegistration;
@@ -23,121 +26,64 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
+    FirebaseUser currentUser;
     ActivityMainBinding binding;
     ListenerRegistration snapshotListener;
+    LinearLayout lista_ingresos, lista_egresos, inicio, log_out;
 
     private static final int TOAST_DURATION = Toast.LENGTH_LONG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        db = FirebaseFirestore.getInstance();
+        lista_ingresos = findViewById(R.id.lista_ingresos_nav);
+        lista_egresos = findViewById(R.id.lista_egresos_nav);
+        inicio = findViewById(R.id.inicio_nav);
+        log_out = findViewById(R.id.cerrar_sesion);
 
-        binding.button.setOnClickListener(view -> {
-            String nombre = binding.textFieldNombre.getEditText().getText().toString();
-            String apellido = binding.textFieldApellido.getEditText().getText().toString();
-            String edadStr = binding.textFieldEdad.getEditText().getText().toString();
-            String dni = binding.textFieldDni.getEditText().getText().toString();
-
-            Usuario usuario = new Usuario(nombre, apellido, Integer.parseInt(edadStr));
-
-            db.collection("usuarios")
-                    .document(dni)
-                    .set(usuario)
-                    .addOnSuccessListener(unused -> {
-                        Toast.makeText(this, "Usuario grabado", TOAST_DURATION).show();
-                    })
-                    .addOnFailureListener(e -> {
-                        Toast.makeText(this, "Algo pasó al guardar ", TOAST_DURATION).show();
-                    });
-        });
-
-
-        binding.btnListarUsuarios.setOnClickListener(view -> {
-            String dni = binding.textFieldDni.getEditText().getText().toString();
-
-            if (!dni.isEmpty()) {
-                binding.btnListarUsuarios.setVisibility(View.GONE);
-                db.collection("usuarios")
-                        .document(dni)
-                        .get()
-                        .addOnCompleteListener(task -> {
-
-                            if (task.isSuccessful()) {
-                                DocumentSnapshot documentSnapshot = task.getResult();
-                                if (documentSnapshot.exists()) {
-                                    Log.d("msg-test", "DocumentSnapshot data: " + documentSnapshot.getData());
-
-                                    Usuario usuario = documentSnapshot.toObject(Usuario.class);
-                                    Toast.makeText(this, String.format("Nombre: %s | apellido: %s",
-                                                    usuario.getNombre(), usuario.getApellido()),
-                                            Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(this, "El usuario no existe", TOAST_DURATION).show();
-                                }
-                            }
-
-                            binding.btnListarUsuarios.setVisibility(View.VISIBLE);
-                        });
-            }
-
-            /*binding.floatingActionButton.setOnClickListener(view2 -> {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-            });*/
-
-        });
-
-        binding.btnTiempoReal.setOnClickListener(view -> {
-
-            snapshotListener = db.collection("usuarios")
-                    .orderBy("edad", Query.Direction.DESCENDING)
-                    .limit(4)
-                    .addSnapshotListener((collection, error) -> {
-
-                        if (error != null) {
-                            Log.w("msg-test", "Listen failed.", error);
-                            return;
-                        }
-
-                        Log.d("msg-test", "---- Datos en tiempo real ----");
-                        for (QueryDocumentSnapshot doc : collection) {
-                            Usuario usuario = doc.toObject(Usuario.class);
-                            Log.d("msg-test",
-                                    "id: " + doc.getId() +
-                                            "| Nombre: " + usuario.getNombre() +
-                                            " | Apellido: " + usuario.getApellido() +
-                                            " | Edad: " + usuario.getEdad());
-                        }
-
-                    });
-        });
-
-        binding.logoutBtn.setOnClickListener(view -> {
-            AuthUI.getInstance().signOut(MainActivity.this)
-                    .addOnCompleteListener(task -> {
-                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                    });
-        });
-
-        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        String correo_usuario = getIntent().getStringExtra("correo");
+        lista_ingresos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+
+                Intent intent = new Intent(MainActivity.this, ingresos_Activity.class);
+                intent.putExtra("correo", correo_usuario); // Reemplaza "clave" y "valor" con la información que quieras pasar
+                startActivity(intent);
+            }
+        });
+        lista_egresos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, egresos_Activity.class);
+                intent.putExtra("correo", correo_usuario); // Reemplaza "clave" y "valor" con la información que quieras pasar
+                startActivity(intent);
+            }
+        });
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Cerrar sesión y redirigir a MainActivity
+                Intent intent = new Intent(MainActivity.this, Login_Activity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
             }
         });
 
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (snapshotListener != null)
-            snapshotListener.remove();
+        db = FirebaseFirestore.getInstance();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = currentUser.getUid();
+
     }
 }
--- app/src/main/java/com/example/la6_20175557/dto/egresos.java
+++ app/src/main/java/com/example/la6_20175557/dto/egresos.java
@@ -1,4 +1,42 @@
 package com.example.la6_20175557.dto;
 
 public class egresos {
+
+    private  String titulo_egreso;
+    private String monto_egreso;
+    private String descripcion_egreso;
+
+    private  String fecha_egreso;
+
+    public String getTitulo_egreso() {
+        return titulo_egreso;
+    }
+
+    public void setTitulo_egreso(String titulo_egreso) {
+        this.titulo_egreso = titulo_egreso;
+    }
+
+    public String getMonto_egreso() {
+        return monto_egreso;
+    }
+
+    public void setMonto_egreso(String monto_egreso) {
+        this.monto_egreso = monto_egreso;
+    }
+
+    public String getDescripcion_egreso() {
+        return descripcion_egreso;
+    }
+
+    public void setDescripcion_egreso(String descripcion_egreso) {
+        this.descripcion_egreso = descripcion_egreso;
+    }
+
+    public String getFecha_egreso() {
+        return fecha_egreso;
+    }
+
+    public void setFecha_egreso(String fecha_egreso) {
+        this.fecha_egreso = fecha_egreso;
+    }
 }
